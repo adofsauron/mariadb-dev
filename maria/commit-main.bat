1c805d88d189e1c00bfe5595753d87916959e26c
adofsauron <adofsauron@163.com>
2022-10-31 22:55:47 +0800

    fix mariadb dev

---
+++ maria/commit-main.bat
@@ -0,0 +1,16 @@
+
+
+git config --global core.autocrlf false
+
+git config  --global core.fsmonitor true
+
+git pull
+
+git add .
+
+git commit -m "fix mariadb dev"
+
+git push -u origin master
+
+REM pause
+
