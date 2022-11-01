77f4432edc6472a2757d75b295d9a312ae7c6eb7
adofsauron <adofsauron@163.com>
2022-11-01 11:15:57 +0800

    fix mariadb dev

--- maria/start.sh
+++ maria/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cd /usr/local/mysql; /usr/local/mysql/bin/mysql_safe --datadir=/usr/local/mysql/var &
+cd /usr/local/mysql; /usr/local/mysql/bin/mysqld_safe --datadir=/usr/local/mysql/var &
 
